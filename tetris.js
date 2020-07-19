678ff4ea72a52143f3f8a5226d4826987a2897e1
rubens-lavor <haryson07@gmail.com>
2020-07-19 16:45:09 -0300

    função desenha quadrado

--- tetris.js
+++ tetris.js
@@ -1,2 +1,18 @@
 const cvs = document.getElementById("tetris")
 const ctx = cvs.getContext("2d")
+
+const ROW = 20
+const COL = COLUMN = 10
+const SQ = squareSize = 20
+const VACANT = "WHITE" // color of an empty square
+
+// draw a square
+function drawSquare(x,y,color) {
+    ctx.fillStyle = color
+    ctx.fillRect(x*SQ, y*SQ, SQ, SQ)
+
+    ctx.strokeStyle = "BLACK"
+    ctx.strokeRect(x*SQ, y*SQ, SQ, SQ)
+}
+
+drawSquare(1,1,"pink")
